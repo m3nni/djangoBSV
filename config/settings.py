32bc99568c287bacaf5b0362bf0ed858e6dfdbec
user1 <bsv.0042@gmail.com>
2023-02-18 19:21:29 +0300

    lesson 3 with trouble

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -63,10 +65,11 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-            ]
+            ],
         },
-    }
+    },
 ]
+
 
 WSGI_APPLICATION = "config.wsgi.application"
 
